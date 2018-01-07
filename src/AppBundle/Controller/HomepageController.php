d9f6ec4efdee385091e69817776a0f53f4cf79e9
maxds98 <Max_D_S@tut.by>
2018-01-07 20:35:17 +0300

    last normal version 07.01.2018

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -14,27 +14,8 @@ class HomepageController extends Controller
      */
     public function homepageAction()
     {
-        $user = $this->getUsers();
-        $quizzes = $this->getQuizzes();
         return $this->render('Homepage/homepage.html.twig', array(
-            'top' => $user,
-            'quizzes' => $quizzes,
+            //...
         ));
-    }
-
-    private function getQuizzes()
-    {
-        $repository = $this->getDoctrine()->getRepository(Quizzes::class);
-        $query = $repository->createQueryBuilder('p')->getQuery();
-        $result = $query->getArrayResult();
-        return $result;
-    }
-
-    private function getUsers()
-    {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        $query = $repository->createQueryBuilder('p')->addOrderBy('p.rating', 'DESC')->getQuery();
-        $user = $query->getArrayResult();
-        return $user;
     }
 }
